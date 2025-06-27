cedf06e30f41c1595da9fcb9434d09b8bbdb3aab
bratAR <adityarauniyar65@gmail.com>
2025-06-27 22:54:18 +0545

    added modals popup

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const scrollBtn = document.getElementById("scrollTopBtn");
+const modal = document.getElementById("modal");
+const openBtn = document.getElementById("openModalBtn");
+const closeBtn = document.getElementById("closeModalBtn");
+
+openBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 300) {
